8924166438d53de1dac10dabf88b53471631c23c
Srikanth21QA <87446681+Srikanth21QA@users.noreply.github.com>
2021-07-16 11:02:34 -0400

    Create TestJava.java
    added a java class

---
+++ src/test/TestJava.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is last java class");
+
+	}
+
+}
